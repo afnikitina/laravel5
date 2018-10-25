98d5fce140be5da2de9289b93277ddf6e509225a
Asya Nikitina <anikitina@cnm.edu>
2018-10-25 14:50:20 -0600

    Added a new view (header.blade.php) and modified the master layout (app.blade.php) and a custom style file (main.css) accordingly.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,12 @@
 <header>
-
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
-
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-md navbar-light bg-light navbar-laravel">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active"><a class="nav-link" href="">Home</a></li>
+                <li class="nav-item active"><a class="nav-link" href="">Blog</a></li>
+                <li class="nav-item active"><a class="nav-link" href="">About</a></li>
+                <li class="nav-item active"><a class="nav-link" href="">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
 </header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <title>@yield('title')</title>
         <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
         @yield('styles')
 
     </head>
     <body>
+        @include('includes.header')
         <div class="container main">
             @yield('content')
         </div>
