67b482bc34684867f637ccd98553a92995a78595
Asya Nikitina <anikitina@cnm.edu>
2018-10-25 00:42:26 -0600

    Created a master layout page (layouts/app.blade.php)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
+        @yield('styles')
+
+    </head>
+    <body>
+        <div class="container main">
+            @yield('content')
+        </div>
+    </body>
+</html>
